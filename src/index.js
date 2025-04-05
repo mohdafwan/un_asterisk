c60ff46d9212b6ccc3f7f55be1fdc20ea637ae71
AFWAN ;) <3xysha@gmail.com>
2025-04-06 01:18:06 +0530

    server listen added

--- src/index.js
+++ src/index.js
@@ -1,8 +1,14 @@
-import { connectDB } from "./db/db.js";
 import 'dotenv/config';
+import express from "express"
+import { connectDB } from "./db/db.js";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
 
 connectDB().then(() => {
-    console.log("MongoDB connected");
+    app.listen(PORT, () => {
+        console.log(`Server is running on port ${PORT}`);
+    });
 }).catch((error) => {
     console.error("MongoDB connection error:", error);
     process.exit(1);
