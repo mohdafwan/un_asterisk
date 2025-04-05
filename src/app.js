3de31c54b16f82b9985a3273f89b8490a126ea6c
AFWAN ;) <3xysha@gmail.com>
2025-04-06 01:17:15 +0530

    setup the important middleware

--- src/app.js
+++ src/app.js
@@ -1,1 +1,16 @@
-import { dotenv } from "dotenv/config.js";
+import express from 'express';
+import cros from 'cors';
+import cookieParser from 'cookie-parser';
+
+const app = express();
+
+//Middleware
+app.use(express.json({limit: '25kb',}));
+app.use(express.urlencoded({extended: true, limit: '25kb',}))
+app.use(express.static('public'));
+app.use(cookieParser());
+
+app.use(cros({
+    origin: process.env.CROSS_ORIGIN || "*",
+    credentials: true,
+}));
